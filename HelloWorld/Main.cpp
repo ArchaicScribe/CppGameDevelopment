b60d30bd9efe682295f1e80fdccb4c855570e4d9
Alex Rauenzahn <xandermv@yahoo.com>
2020-08-17 13:52:08 -0600

    Primitve types.

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -3,6 +3,20 @@
 void Log(const char* message);
 
 int main() {
-	Log("Hello World");
+
+	unsigned int variable = 8; //-2b -> 2b
+
+	std::cout << variable << std::endl;
+	variable = 20;
+	std::cout << variable << std::endl;
 	std::cin.get();
+
+	float z = 5.5f;
+	std::cout << z << std::endl;
+
+	double x = 5.2;
+	std::cout << x << std::endl;
+
+	bool yesOrNo = true;
+	std::cout << sizeof(yesOrNo) << std::endl;
 }
