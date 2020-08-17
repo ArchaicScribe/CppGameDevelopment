148dadacf0724be2531049bc3a4e7c1f526c6333
Alex Rauenzahn <xandermv@yahoo.com>
2020-08-17 14:41:03 -0600

    Basic functions.

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 
-void Log(const char* message);
+
+int Multiply(int a, int b)
+{
+	return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+	int result = Multiply(a,b);
+	std::cout << result << std::endl;
+}
 
 int main() {
 
-	unsigned int variable = 8; //-2b -> 2b
-
-	std::cout << variable << std::endl;
-	variable = 20;
-	std::cout << variable << std::endl;
+	MultiplyAndLog(3, 2);
+	MultiplyAndLog(8, 5);
+	MultiplyAndLog(90, 45);
 	std::cin.get();
-
-	float z = 5.5f;
-	std::cout << z << std::endl;
-
-	double x = 5.2;
-	std::cout << x << std::endl;
-
-	bool yesOrNo = true;
-	std::cout << sizeof(yesOrNo) << std::endl;
 }
