6b3e0bd451b1ee23a6c2317a50cc21981aba2ff3
Alex Rauenzahn <xandermv@yahoo.com>
2020-10-06 12:21:39 -0600

    Added in basic variables, sum, and product results.

---
+++ TripleX/TripleX/TripleX/TripleX.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "A small child with great force powers is near you. You can sense it";
+	std::cout << std::endl;
+	std::cout << "Enter the correct code to continue...";
+
+	int a = 3997;
+	int b = 3642;
+	int c = 3638;
+
+	int sum = a + b + c;
+	int product = a * b * c;
+
+	std::cout << std::endl;
+	std::cout << sum;
+
+	std::cout << std::endl;
+	std::cout << product << std::endl;
+
+	return 0;
+} 
