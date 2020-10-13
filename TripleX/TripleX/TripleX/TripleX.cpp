d857c334fda3df36ed4c453f29ee14e46b678749
Alex Rauenzahn <xandermv@yahoo.com>
2020-10-13 16:30:34 -0600

    Changed naming convention to match UE4's

--- TripleX/TripleX/TripleX/TripleX.cpp
+++ TripleX/TripleX/TripleX/TripleX.cpp
@@ -1,26 +1,30 @@
+//Prepocessor statements
+//Following Unreal Engine 4's UpperCamelCase naming convention. 
 #include <iostream>
+#include "TripleX.h"
 
 int main()
 {
-
+	//Printed out statements
 	std::cout << "A small child with great force powers is near you. You can sense it";
 	std::cout << std::endl;
 	std::cout << "Enter the correct code to continue...";
 
-	const int a = 3997;
-	const int b = 3;
-	const int c = 3;
+
+	//Declare 3 number code
+	const int CodeA = 3997;
+	const int CodeB = 3;
+	const int CodeC = 3;
 
 
-	const int sum = a + b + c;
-	const int product = a * b * c;
+	const int CodeSum = CodeA + CodeB + CodeC;
+	const int CodeProduct = CodeA + CodeB + CodeC; 
 	
-
+	//Print sum and product to the terminal 
 	std::cout << std::endl;
-	std::cout << sum;
-
-	std::cout << std::endl;
-	std::cout << product << std::endl;
+	std::cout << "There are 3 numbers in the code\n" << std::endl;
+	std::cout << "The code adds up to: " << CodeSum << std::endl;
+	std::cout << "The code multiplied equals: " << CodeProduct << std::endl;
 
 	return 0;
 } 
